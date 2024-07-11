b7dd13f2d3e7f3d196fb2a4f9f95ae9a3ffaec2e
Kunal Kurve <165116100+KunalKurve@users.noreply.github.com>
2024-07-12 00:22:13 +0530

    Add files via upload

---
+++ Official Time Prediction/02 Official Time Predict Flask.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+model = joblib.load("final_time_model.pkl")
+col_names = joblib.load("pace_column_names.pkl")
+
+@app.route('/')
+def hello() -> str:
+    return 'Hello Pace..... from Flask!'
+
+@app.route('/predictpace', methods=['POST'])
+def predict():
+    
+    #Get JSON Request
+    user_data = request.json
+    
+    #Convert JSON request to Panadas DataFrame
+    df = pd.DataFrame(user_data)
+    
+    #Match Column Names
+    df = df.reindex(columns=col_names)
+    
+    #Get prediction
+    prediction = list(model.predict(df))
+    
+    #Return JSON version of Prediction
+    return jsonify({'prediction' : str(prediction)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
